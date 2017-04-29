c93e5204770b5d7896e4a80124b3d258aacc41fc
Vince Knight <vincent.knight@gmail.com>
2017-04-29 12:21:32 +0100

    Minor reorganisation of files.

---
+++ src/test.sh
@@ -0,0 +1,4 @@
+echo "Testing moran.py"
+python -m unittest moran.py
+echo "Testing theoretic.py"
+python -m unittest theoretic.py
--- test.sh
+++
@@ -1,4 +0,0 @@
-echo "Testing approximate_moran.py"
-python -m unittest approximate_moran.py
-echo "Testing moran.py"
-python -m unittest moran.py
